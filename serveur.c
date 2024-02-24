251f593bca1f0936e9a273873c39033c043d0971
arnaud-feldmann <arnaud.feldmann@gmail.com>
2024-02-24 10:12:44 +0100

    refactor

--- serveur.c
+++ serveur.c
@@ -17,8 +17,23 @@
 #define FALSE 0
 typedef int bool;
 
-/* Les tampons servent à construire le header. Ils prenent le pointeur vers le rep de réponse et une longueur,
- * écrivent leur truc et incrémentent la longueur du header */
+static int sockfd_ecoute; /* Variable globale uniquement pour pouvoir la fermer en réponse à sigint/sigterm */
+static regex_t regex_requete_get; /* Variable globale pour ne la compiler qu'une fois */
+
+void stop_si(bool condition, const char* message_perror) {
+    if (condition) {
+        perror(message_perror);
+        exit(EXIT_FAILURE);
+    }
+}
+
+void handler_sigint_sigterm(__attribute__((unused)) int sig) {
+    close(sockfd_ecoute);
+    chdir("..");
+    exit(0);
+}
+
+/* Fonctions de construction du header HTTP/1.1 200 OK */
 
 void tampon_fixed(char* rep, int* rep_len) {
     char* header_fixed = "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\nServer: ArnaudHTTP\r\nConnection: close\r\n";
@@ -53,17 +68,51 @@ int make_header(char* rep, FILE* fichier) {
     return rep_len;
 }
 
-void erreur_si(bool condition, const char* message_perror) {
-    if (condition) {
-        perror(message_perror);
-        exit(EXIT_FAILURE);
+/* Construction de la réponse du serveur. req est supposé existant */
+
+void make_rep(char* req, char* rep) {
+    regmatch_t matches[4];
+    if (regexec(&regex_requete_get, req, 4, matches, 0) == 0) {
+        char vermaj = req[matches[2].rm_so];
+        char vermin = req[matches[3].rm_so];
+        if (vermaj == '1' && vermin == '1') {
+            FILE *fichier;
+            int nom_html_match = matches[1].rm_so;
+            int nom_html_longueur = matches[1].rm_eo - nom_html_match;
+            char nom_html[BUFFER_LEN];
+            if (nom_html_longueur == 0) strcpy(nom_html,"index.html");
+            else {
+                strncpy(nom_html,req+nom_html_match,nom_html_longueur);
+                for(int i = 0; nom_html[i]; i++) nom_html[i] = (char)tolower(nom_html[i]);
+            }
+            fichier = fopen(nom_html, "r");
+            if (fichier == NULL) strcpy(rep,"HTTP/1.1 404 Not Found\r\nContent-Length: 13\r\n\r\n404 Not Found");
+            else {
+                int rep_len = make_header(rep,fichier);
+                for ( ; rep_len < BUFFER_LEN - 1 ; rep_len++) {
+                    int temp_char = fgetc(fichier);
+                    if (temp_char == EOF) break;
+                    rep[rep_len]=(char)temp_char;
+                }
+                rep[rep_len]='\0';
+                fclose(fichier);
+            }
+        }
+        else {
+            strcpy(rep,"HTTP/1.1 505 HTTP Version Not Supported\r\nContent-Length: 0\r\n\r\n");
+            rep[5]=vermaj;
+            rep[7]=vermin;
+        }
     }
+    else strcpy(rep,"HTTP/1.1 501 Not Implemented\r\nContent-Length: 0\r\n\r\n");
 }
+
+/* Fonctions relatives à la mnipulation de socket */
 
 void reuseaddr(int sockfd) {
     const int enable = 1;
-    erreur_si(setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) < 0,
-              "setsockopt(SO_REUSEADDR)");
+    stop_si(setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) < 0,
+            "setsockopt(SO_REUSEADDR)");
 }
 
 void bind_port(int sockfd,uint16_t port) {
@@ -72,7 +121,7 @@ void bind_port(int sockfd,uint16_t port) {
     adresse.sin_family=AF_INET;
     adresse.sin_port= htons(port);
     adresse.sin_addr.s_addr=INADDR_ANY;
-    erreur_si(bind(sockfd, (struct sockaddr *) &adresse, sizeof(adresse)), "bind");
+    stop_si(bind(sockfd, (struct sockaddr *) &adresse, sizeof(adresse)), "bind");
 }
 
 void socket_timeout(int sockfd) {
@@ -82,80 +131,38 @@ void socket_timeout(int sockfd) {
     setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, (const char*)&tv, sizeof tv);
 }
 
-int sockfd;
-
-void handler_sigterm(__attribute__((unused)) int sig) {
-    close(sockfd);
-    chdir("..");
-    exit(0);
-}
-
 int main() {
     char req[BUFFER_LEN];
     char rep[BUFFER_LEN];
     bool continuer_session = TRUE;
-    sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    erreur_si(socket < 0, "socket");
-    socket_timeout(sockfd);
-    bind_port(sockfd, PORT);
-    listen(sockfd, 15);
+    sockfd_ecoute = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    stop_si(socket < 0, "socket");
+    socket_timeout(sockfd_ecoute);
+    bind_port(sockfd_ecoute, PORT);
+    listen(sockfd_ecoute, 15);
     struct sockaddr client_adr;
     socklen_t client_addrlen;
-    regex_t regex;
-    regmatch_t matches[4];
-    erreur_si(regcomp(&regex, "^GET\\s.*\\/([a-z]+\\.html)?\\sHTTP\\/([0-9])\\.([0-9])", REG_EXTENDED|REG_ICASE),
-              "regex");
-    signal(SIGINT,handler_sigterm);
-    signal(SIGTERM,handler_sigterm);
+    stop_si(regcomp(&regex_requete_get, "^GET\\s.*\\/([a-z]+\\.html)?\\sHTTP\\/([0-9])\\.([0-9])", REG_EXTENDED | REG_ICASE),
+            "regex_requete_get");
+    signal(SIGINT, handler_sigint_sigterm);
+    signal(SIGTERM, handler_sigint_sigterm);
     chdir("static");
     setlocale(LC_ALL, "C"); // Pour que strftime soit en anglais
     while (TRUE) {
-        int session_sockfd = accept(sockfd, &client_adr, &client_addrlen);
+        int sockfd_session = accept(sockfd_ecoute, &client_adr, &client_addrlen);
         if (fork() == 0) {
-            close(sockfd);
+            close(sockfd_ecoute);
             while (continuer_session) {
                 memset(req,0,BUFFER_LEN);
-                long bytes = recv(session_sockfd,&req,BUFFER_LEN,0);
+                long bytes = recv(sockfd_session, &req, BUFFER_LEN, 0);
                 if (bytes > 0) {
-                    if (regexec(&regex, req, 4, matches, 0) == 0) {
-                        char vermaj = req[matches[2].rm_so];
-                        char vermin = req[matches[3].rm_so];
-                        if (vermaj == '1' && vermin == '1') {
-                            FILE *fichier;
-                            int nom_html_match = matches[1].rm_so;
-                            int nom_html_longueur = matches[1].rm_eo - nom_html_match;
-                            char nom_html[BUFFER_LEN];
-                            if (nom_html_longueur == 0) strcpy(nom_html,"index.html");
-                            else {
-                                strncpy(nom_html,req+nom_html_match,nom_html_longueur);
-                                for(int i = 0; nom_html[i]; i++) nom_html[i] = (char)tolower(nom_html[i]);
-                            }
-                            fichier = fopen(nom_html, "r");
-                            if (fichier == NULL) strcpy(rep,"HTTP/1.1 404 Not Found\r\nContent-Length: 13\r\n\r\n404 Not Found");
-                            else {
-                                int rep_len = make_header(rep,fichier);
-                                for ( ; rep_len < BUFFER_LEN - 1 ; rep_len++) {
-                                    int temp_char = fgetc(fichier);
-                                    if (temp_char == EOF) break;
-                                    rep[rep_len]=(char)temp_char;
-                                }
-                                rep[rep_len]='\0';
-                                fclose(fichier);
-                            }
-                        }
-                        else {
-                            strcpy(rep,"HTTP/1.1 505 HTTP Version Not Supported\r\nContent-Length: 0\r\n\r\n");
-                            rep[5]=vermaj;
-                            rep[7]=vermin;
-                        }
-                    }
-                    else strcpy(rep,"HTTP/1.1 501 Not Implemented\r\nContent-Length: 0\r\n\r\n");
-                    send(session_sockfd, rep, strlen(rep) + 1, 0);
+                    make_rep(req,rep);
+                    send(sockfd_session, rep, strlen(rep) + 1, 0);
                 }
                 else continuer_session = FALSE;
             }
-            close(session_sockfd);
+            close(sockfd_session);
             return 0;
-        } else close(session_sockfd);
+        } else close(sockfd_session);
     }
 }
